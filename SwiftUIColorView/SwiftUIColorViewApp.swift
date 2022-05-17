c9606bf0a2520d9fc9577f774041bca385f93e6e
venzhi87 <anarhy87@list.ru>
2022-05-17 18:19:00 +0300

    Initial Commit

---
+++ SwiftUIColorView/SwiftUIColorViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIColorViewApp.swift
+//  SwiftUIColorView
+//
+//  Created by Pavel Kuzovlev on 17.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIColorViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
